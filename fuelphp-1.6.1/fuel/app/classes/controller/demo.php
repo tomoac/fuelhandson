d43e37baa5b7a57b0fbde24f20fe8e6476bdce9e
kinoppy <kinoshitatetsuo@KinoshitaMacBookAir.local>
2013-07-20 15:22:56 +0900

    action_select

--- fuelphp-1.6.1/fuel/app/classes/controller/demo.php
+++ fuelphp-1.6.1/fuel/app/classes/controller/demo.php
@@ -9,11 +9,28 @@ class Controller_Demo extends Controller{
    
     function action_showdata()
     {
-        $data = array('text'=>'Nagoya');
-
+        $data = array('text'=>'Nagoya', 'hoge'=>'Tokyo!');
         return Response::forge(View::forge('demo/showdata',$data));
     }
     
+    function action_select($id = null)
+    {
+        $list = array(
+            1 => '1st',
+            2 => '2st',
+            3 => '3st'
+        );
+        
+        if($id === null) $id = 1;
+        else if($id > 3) $id = 3;
+        
+        $data = array('text' => $list[$id]);
+        return Response::forge(View::forge('demo/select',$data));
+        
+        
+       
+        
+    }
 }
 
 
